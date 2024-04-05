e26db6d010aa112a304d047fde1f50246deb4434
Steven Stansberry <stevenpstansberry@gmail.com>
2024-04-05 15:18:54 -0700

    added camera capture

---
+++ main.py
@@ -0,0 +1,29 @@
+import cv2
+
+
+capture = cv2.Videocapture(0)
+
+# Check if the camera opened successfully
+if not capture.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+# Continuously capture frames from the camera
+while True:
+    ret, frame = capture.read()
+
+    # If frame is read correctly, ret is True
+    if not ret:
+        print("Error: Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Webcam', frame)
+
+    # Break the loop when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+capture.release()
+cv2.destroyAllWindows()
